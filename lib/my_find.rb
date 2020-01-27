c15d2cf28add6d0551d40a28d2ffbefcf6c3d159
Sara Isabel Garcia Reyes <sarisgar28@gmail.com>
2020-01-27 21:49:04 +0000

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,15 @@
 require 'pry'
-
+collection = (1..100).to_a 
+my_find(collection) {|i| i % 3 == 0 and i % 5 ==0}
 def my_find(collection)
-
-end
+  i= 0 
+  while i < 100
+  yield(1)
+  collection.lentgh 
+  yield (collection[i])
+  i = i + 1 
+end 
+i = i + 1
+ end
+ true 
+ end
